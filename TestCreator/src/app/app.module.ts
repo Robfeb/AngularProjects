8f1cc7498b39e8e54af6e76c3897aff1ffb416f6
Roberto Febrer <febrer@gmail.com>
2020-11-08 00:04:52 +0100

    add radio question

--- TestCreator/src/app/app.module.ts
+++ TestCreator/src/app/app.module.ts
@@ -6,16 +6,19 @@ import { AppComponent } from './app.component';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 import { QuestionComponent } from './question/question.component';
 import { SelectQuestionComponent } from './select-question/select-question.component';
 import {MatIconModule} from '@angular/material/icon';
 import { SingleSelectQuestionComponent } from './single-select-question/single-select-question.component';
+import { RadioQuestionComponent } from './radio-question/radio-question.component';
 @NgModule({
   declarations: [
     AppComponent,
     QuestionComponent,
     SelectQuestionComponent,
     SingleSelectQuestionComponent,
+    RadioQuestionComponent,
   ],
   imports: [
     BrowserAnimationsModule,
@@ -25,7 +28,8 @@ import { SingleSelectQuestionComponent } from './single-select-question/single-s
     MatSelectModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
