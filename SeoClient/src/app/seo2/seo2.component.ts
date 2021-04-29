d5b435dedfd01ef8f3362e10c022bcae9c2d1389
Roberto Febrer <febrer@gmail.com>
2021-04-29 09:34:16 +0200

    add script to download

--- SeoClient/src/app/seo2/seo2.component.ts
+++ SeoClient/src/app/seo2/seo2.component.ts
@@ -14,35 +14,35 @@ export class Seo2Component implements OnInit {
 
   postList: String[] = [];
   constructor(private textService:TextService) {
-    this.loadAPI = new Promise((resolve) => {
-      this.loadScript();
-      resolve(true);
-  });
+  //   this.loadAPI = new Promise((resolve) => {
+  //     this.loadScript();
+  //     resolve(true);
+  // });
   }
   ngOnInit(): void {
     this.postList=this.textService.getPostList(5,10);
   }
-  public loadScript() {
-    var isFound = false;
-    var scripts = document.getElementsByTagName("script")
-    for (var i = 0; i < scripts.length; ++i) {
-        if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
-            isFound = true;
-        }
-    }
+  // public loadScript() {
+  //   var isFound = false;
+  //   var scripts = document.getElementsByTagName("script")
+  //   for (var i = 0; i < scripts.length; ++i) {
+  //       if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
+  //           isFound = true;
+  //       }
+  //   }
 
-    if (!isFound) {
-        var dynamicScripts = ["https://seoservice.azurewebsites.net/api/Seo?token=123456"];
+  //   if (!isFound) {
+  //       var dynamicScripts = ["https://seoservice.azurewebsites.net/api/Seo?token=123456"];
 
-        for (var i = 0; i < dynamicScripts.length; i++) {
-            let node = document.createElement('script');
-            node.src = dynamicScripts [i];
-            node.type = 'text/html';
-            node.async = false;
-            node.charset = 'utf-8';
-            document.getElementsByTagName('head')[0].appendChild(node);
-        }
+  //       for (var i = 0; i < dynamicScripts.length; i++) {
+  //           let node = document.createElement('script');
+  //           node.src = dynamicScripts [i];
+  //           node.type = 'text/html';
+  //           node.async = false;
+  //           node.charset = 'utf-8';
+  //           document.getElementsByTagName('head')[0].appendChild(node);
+  //       }
 
-    }
-}
+  //   }
+//}
 }
